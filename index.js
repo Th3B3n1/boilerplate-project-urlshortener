5861e6d136527658af96818f44ec687922ad6dd0
Th3B3n1 <othersc9bst@gmail.com>
2024-10-05 22:30:54 +0200

    done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+const url = require('url')
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const jsonParser = bodyParser.urlencoded({extended: false});
+const submittedUrls = new Array({original_url: "https://google.com", short_url: 40});
 
 app.use(cors());
 
@@ -18,6 +23,38 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', jsonParser, function (req, res)
+{
+  if (req.body.url != undefined)
+  {
+    try
+    {
+      new URL(req.body.url);
+      submittedUrls.push({original_url: req.body.url, short_url: submittedUrls.length + 1})
+      res.json({original_url: req.body.url, short_url: submittedUrls.length});
+    }
+    catch
+    {
+      res.json({error:"Invalid URL"});
+    }
+  }
+  else
+  {
+    res.json({error:"Invalid URL"});
+  }
+})
+
+app.get("/api/shorturl/:short_url", jsonParser, function (req, res)
+{
+  for (let i = 0; i < submittedUrls.length; i++)
+  {
+    if (submittedUrls[i].short_url == req.params.short_url)
+    {
+      res.redirect(submittedUrls[i].original_url);
+    }
+  }
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
